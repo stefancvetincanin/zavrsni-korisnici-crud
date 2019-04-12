fa6651fb310bc10fe232c20250c93263cc20f910
stefancvetincanin <stefancvetincanin@gmail.com>
2019-04-12 13:36:42 +0200

    Updates

---
+++ precache-manifest.840879fd8cc97cf048bc8a4ca967406c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265bbd5fd4f160a4950b",
+    "url": "/zavrsni-korisnici-crud/static/js/runtime~main.3d64908d.js"
+  },
+  {
+    "revision": "eea741be6d5d3365f395",
+    "url": "/zavrsni-korisnici-crud/static/js/main.40d59d14.chunk.js"
+  },
+  {
+    "revision": "ef78a1efa7a52feb065a",
+    "url": "/zavrsni-korisnici-crud/static/js/2.6bd97686.chunk.js"
+  },
+  {
+    "revision": "eea741be6d5d3365f395",
+    "url": "/zavrsni-korisnici-crud/static/css/main.fc28ae1c.chunk.css"
+  },
+  {
+    "revision": "b6cc949d1e6fa1c82b2c5335f31f45d0",
+    "url": "/zavrsni-korisnici-crud/index.html"
+  }
+];
