8c65211e43d2690dfc80d4025f7c65fde9c74a01
stefancvetincanin <stefancvetincanin@gmail.com>
2019-04-28 00:19:33 +0200

    Updates

---
+++ precache-manifest.ca16de9c82b3eeadf52b5b21004c9ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ba725daacaf4be4af44f22da092452b",
+    "url": "/zavrsni-korisnici-crud/static/media/loader.7ba725da.gif"
+  },
+  {
+    "revision": "265bbd5fd4f160a4950b",
+    "url": "/zavrsni-korisnici-crud/static/js/runtime~main.3d64908d.js"
+  },
+  {
+    "revision": "4949b6739cf16e688d94",
+    "url": "/zavrsni-korisnici-crud/static/js/main.5f3ee791.chunk.js"
+  },
+  {
+    "revision": "d3b7854560f1db0c9468",
+    "url": "/zavrsni-korisnici-crud/static/js/2.d04f845d.chunk.js"
+  },
+  {
+    "revision": "4949b6739cf16e688d94",
+    "url": "/zavrsni-korisnici-crud/static/css/main.28aff88c.chunk.css"
+  },
+  {
+    "revision": "3065b778d9e26c992362306a6747d6ac",
+    "url": "/zavrsni-korisnici-crud/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/zavrsni-korisnici-crud/precache-manifest.fd13fba89d68cd50ac769e26545f3bee.js"
+  "/zavrsni-korisnici-crud/precache-manifest.ca16de9c82b3eeadf52b5b21004c9ce0.js"
 );
 
 workbox.clientsClaim();
